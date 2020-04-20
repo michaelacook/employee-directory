88e637f4c00bb4a408bd41f895cac53b7cd9732b
Michael Cook <mcook0775@gmail.com>
2020-04-20 16:51:36 -0400

    Alter styles, remove html comments, improve functionality

--- js/scripts.js
+++ js/scripts.js
@@ -7,8 +7,11 @@ by Michael Cook
 // API docs: https://randomuser.me/documentation
 
 // Globals 
-let employeesArr;
+let employeesData;
 const gallery = document.getElementById('gallery');
+
+
+// ---------------------------------------------- BASIC FUNCTIONALITY ---------------------------------------------- //
 
 
 /**
@@ -21,7 +24,7 @@ const createEmployeeCard = employee => {
     cardDiv.className = 'card';
     cardInnerHTML = `
         <div class="card-img-container">
-            <img class="card-img" src="${employee.picture.thumbnail}" alt="profile picture">
+            <img class="card-img" src="${employee.picture.large}" alt="profile picture">
         </div>
         <div class="card-info-container">
             <h3 id="name" class="card-name cap">${employee.name.first} ${employee.name.last}</h3>
@@ -39,15 +42,15 @@ const createEmployeeCard = employee => {
 
 
 /**
- * Create an array of employee objects and assign to the employeesArr variable 
- * The employeesArr array is used to maintain global data for generating modals dynamically
+ * Create an array of employee objects and assign to the employeesData variable 
+ * The employeesData array is used to maintain global data for generating modals dynamically
  * @param {Array} results - destructured from employee data 
  */
-const assignEmployeesArr = ({ results }) => {
+const assignEmployeesData = ({ results }) => {
     const output = new Array();
     results.forEach(result => {
         const empObj = new Object();
-        empObj.img = result.picture.thumbnail;
+        empObj.img = result.picture.large;
         empObj.name = `${result.name.first} ${result.name.last}`;
         empObj.email = result.email;
         empObj.city = result.location.city;
@@ -56,7 +59,7 @@ const assignEmployeesArr = ({ results }) => {
         empObj.birthday = `Birthday: ${result.dob.date.slice(0, 10).replace(/-/g, '/')}`;
         output.push(empObj);
     });
-    employeesArr = output;
+    employeesData = output;
 }
 
 
@@ -64,7 +67,6 @@ const assignEmployeesArr = ({ results }) => {
  * Add a click event listener to each card that calls createModal onclick
  * This function is written to allow the user to click anywhere in the card 
  * Depending on where the user clicks, the function uses DOM traversal to get the outermost card div and pass it to createModal
- * I don't know if this function is written in the most efficient way, but I couldn't figure out another way to do it
  */
 const addCardEventListeners = () => {
     const cards = Array.from(document.querySelectorAll('.card'));
@@ -91,16 +93,16 @@ const addCardEventListeners = () => {
 /**
  * For each employee object, create and append a card div to the page
  * Function destructures the results array out of the data object
- * Calls assignEmployeesArr 
+ * Calls assignemployeesData and addCardEventListeners 
+ * Rationale for calling these functions here is they don't return promises, so calling them inside displayEmployeeCards instead of in a "then" method makes more sense
  * @param {Array} data - array of employee data objects 
  */
 const displayEmployeeCards = ({ results }) => {
-    const gallery = document.getElementById('gallery');
     results.forEach(employee => {
         const employeeCard = createEmployeeCard(employee);
         gallery.appendChild(employeeCard);
     });
-    assignEmployeesArr({ results });
+    assignEmployeesData({ results });
     addCardEventListeners();
 }
 
@@ -118,7 +120,7 @@ const handleError = err => {
 
 
 // Asynchronously fetch data from the API
-fetch('https://randomuser.me/api/?results=12')
+fetch('https://randomuser.me/api/?results=12&nat=ca,au,nz,us')
     .then(res => res.json())
     .then(data => displayEmployeeCards(data))
     .catch(err => handleError(err));
@@ -128,15 +130,18 @@ fetch('https://randomuser.me/api/?results=12')
 
 
 /**
- * Generate a modal element for an employee
+ * Generate a modal element for an employee card
  * Function destructures the target object from the event object
- * @param {Number} el - event object from click
+ * This function works by getting the index of the clicked card in the collection of card elements, and using that
+ * index to get the data for the employee from the employeesData array, which it uses to populate the modal
+ * I believe this is a cleaner solution than getting the employee data from the textContent properties of the card
+ * @param {Number} el - employee card element clicked by user
  */
 function createModal(el) {
     removeModal();
     const cards = Array.from(document.querySelectorAll('.card'));
     const index = cards.indexOf(el);
-    const employee = employeesArr[index];
+    const employee = employeesData[index];
     const modal = document.createElement('div');
     modal.setAttribute('data-employee-index', index);
     modal.className = 'modal-container';
@@ -178,13 +183,126 @@ const removeModal = () => {
  * @param {Bool} next - true to next, false to navigate to previous
  */
 function navigateEmployees(next) {
-    let index = document.querySelector('.modal-container')
+    const modal = document.querySelector('.modal');
+    if (modal) {
+        let index = document.querySelector('.modal-container')
         .getAttribute('data-employee-index');
-    if (next && index < 11) {
-        index++;
-    } else if (!next && index > 0) {
-        index--;
+        if (next && index < 11) {
+            index++;
+        } else if (!next && index > 0) {
+            index--;
+        }
+        const employee = Array.from(gallery.children)[index];
+        createModal(employee);
     }
-    const employee = Array.from(gallery.children)[index];
-    createModal(employee);
 }
+
+
+/**
+ * Allow modal to be closed by pressing the ESC key
+ * Allow user to navigate employees using arrow keys
+ */
+document.addEventListener('keydown', e => {
+    if (e.keyCode === 27) {
+        removeModal();
+    } else if (e.keyCode === 37) {
+        navigateEmployees(false);
+    } else if (e.keyCode === 39) {
+        navigateEmployees(true);
+    }
+});
+
+
+// ---------------------------------------------- SEARCH ---------------------------------------------- //
+
+
+/**
+ * Add the search form dynamically so that if JS isn't enabled, the user will not see a search bar that lacks functionality
+ */
+document.querySelector('.search-container').innerHTML = `
+<form action="#" method="get">
+    <input type="search" id="search-input" class="search-input" placeholder="Search...">
+    <input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
+</form>`;
+
+
+/**
+ * Append a no results message to the page header
+ */
+const noResultsMessage = () => {
+    const header = document.querySelector('.header-text-container');
+    const h3 = document.createElement('h3');
+    h3.setAttribute('id', "no-results-message");
+    h3.textContent = "No results";
+    header.appendChild(h3);
+}
+
+
+/**
+ * Remove no results message from the DOM
+ */
+const removeNoResultsMessage = () => {
+    const msg = document.getElementById('no-results-message');
+    if (msg) msg.remove();
+}
+
+
+/**
+ * Determine if there are no results of a search, and if no results call noResultsMessage function
+ * @param {Array} employeeCards - array of employee cards passed from the filter function
+ */
+const checkForNoResults = (employeeCards) => {
+    for (let card of employeeCards) {
+        if (window.getComputedStyle(card).display === "flex") {
+            return;
+        }
+    }
+    noResultsMessage();
+}
+
+
+/**
+ * Filter employees by setting non-matches to display:none
+ * Call removeNoResultsMessage so that if there are results of a search the message does not remain
+ * @param {String} searchKeys - keys inputted by user to search for an employee 
+ */
+const filter = (searchKeys) => {
+    removeNoResultsMessage();
+    const employeeCards = Array.from(document.querySelectorAll('.card'));
+    for (let i = 0; i < employeesData.length; i++) {
+        if (!employeesData[i].name.toLowerCase().includes(searchKeys)) {
+            employeeCards[i].style.display = "none";
+        } else {
+            employeeCards[i].style.display = "flex";
+        }
+    } 
+    checkForNoResults(employeeCards);
+}
+
+const searchBtn = document.getElementById('search-submit');
+const searchInput = document.getElementById('search-input');
+
+
+/**
+ * Listen for clicks on the search button, call the filter function and pass the searchInput value
+ */
+searchBtn.addEventListener('click', e => {
+    // prevent form submission since the form does not need to actually send a server request
+    e.preventDefault();
+    // unfocus the search button after clicking
+    searchBtn.blur();
+    const searchKeys = searchInput.value;
+    filter(searchKeys);
+});
+
+
+/**
+ * Listen for the 'input' event, filter based on search keys entered
+ * The 'input' event is used instead of keyup or keydown because this event fires 
+ * whenever the value of the input is changed, allowing the filter function to be called 
+ * when the user clicks the X supplied by the user agent
+ */
+searchInput.addEventListener('input', e => {
+    const searchKeys = searchInput.value;
+    filter(searchKeys);
+});
